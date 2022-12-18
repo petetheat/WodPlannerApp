14d7690fbddd47aa65abe8b1fb99426f33436a59
Pete <p.eisenschmidt@gmail.com>
2022-12-18 13:53:15 +0100

    DB Update + change static folder

--- carabaowod/settings.py
+++ carabaowod/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5kz36@13c7so^6-9ah@_6_b6)2!-9jqgxlqlu#ek09owi)ng@t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.57', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.57', '127.0.0.1', 'petetheat.eu.pythonanywhere.com']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
